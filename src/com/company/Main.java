f31f84f22777658152aaf0aaad20a321e26e3c2b
loc90s <loc90@rambler.ru>
2021-08-26 13:09:52 +0400

    commit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,6 +4,11 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        System.out.print("Привет мир!");
+        System.out.println("                 РУБАИ");
+        System.out.println("Много лет размышлял я над жизнью земной.");
+        System.out.println("Непонятного нет для меня под луной.");
+        System.out.println("Мне известно, что мне ничего не известно!");
+        System.out.println("Вот последняя правда, открытая мной.");
+        System.out.println("                                О.Хайям");
     }
 }
